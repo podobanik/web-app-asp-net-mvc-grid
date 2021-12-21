5c9e87f3eab72a725e7dd8ce8fe424d6ff345501
root <green.grocer@mail.ru>
2021-12-22 02:32:33 +1000

    Init

---
+++ WebAppAspNetMvcGrid/Common/Models/Grid/GridCellActionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Common.Models
+{
+    public class GridCellActionViewModel
+    {
+        public int Id { get; set; }
+        public string ControllerName { get; set; }
+    }
+}
---
+++ WebAppAspNetMvcGrid/WebAppAspNetMvcGrid/Models/Enums/SettingType.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppAspNetMvcGrid.Models
+{
+    public enum SettingType
+    {
+        [Display(Name = "Пароль")]
+        Password = 1,
+    }
+}
